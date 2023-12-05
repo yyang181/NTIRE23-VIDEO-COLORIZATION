cede7d96d7ce7c695d41cbd6ca191bcfdeb13c38
yyang <yixin.yang@email.ucr.edu>
2023-12-05 15:24:51 +0800

    uncomment openxlab code

--- BiSTNet-NTIRE2023/app.py
+++ BiSTNet-NTIRE2023/app.py
@@ -24,7 +24,7 @@ import os
 #     os.system("unzip models.zip")
 
 # for openxlab codes
-os.system("cd ./NTIRE23-VIDEO-COLORIZATION/BiSTNet-NTIRE2023/")
+os.system("cd NTIRE23-VIDEO-COLORIZATION/BiSTNet-NTIRE2023/")
 os.system("pip3 uninstall torch -y")
 # os.system("pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1")
 os.system("pip3 install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113")
